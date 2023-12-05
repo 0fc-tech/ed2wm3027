3a7078380a7f71102c77c6f938181f42e9ec3212
riderofzion <quentinrouet@hotmail.fr>
2023-12-05 16:52:33 +0100

    Correction TP Module 4

--- enishop/src/main/java/com/eniecole/enishop/MainActivity.kt
+++ enishop/src/main/java/com/eniecole/enishop/MainActivity.kt
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.eniecole.enishop.dao.ArticleRepository
+import com.eniecole.enishop.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
 //logt
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.article = ArticleRepository.getArticle(1L)
         //logd logi
         Log.d(TAG, "onCreate: ${ArticleRepository.getArticle(1L)}")
+        binding.button.setOnClickListener {
+            Snackbar.make(it,"Vous venez de créer ${binding.article?.titre} vendu pour un" +
+                    "montant de ${binding.article?.prix}€",Snackbar.LENGTH_LONG).show()
+        }
     }
 }
